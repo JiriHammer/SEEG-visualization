404bc04c33d9448c11f7b82bae4946f6299de89a
kamilvlcek <kamil@biomed.cas.cz>
2017-03-30 11:06:08 +0200

    SEEG-Vizualization - upravy main_brainPlot
    - vystupni cesta plotInfo.outputDir se zadava absolutne, takze nemeni pracovni adresar
    - nenastavuje plotInfo.model_views, uz se v plot_brain3D  nepouziva
    - nove parametry plotInfo. savefig, savepng, circle_size
    - promenne data_channels a vals prejmenovany na mni_channels a vals_channels, kvuli srozumitelnosti
    -- oboji musi byt nastaveno zvenku
    - pokud existuje promenna brainsurface, kde je drive spocitany isosurface, vlozi ji do plotInfo.fv (to se dal pouziva v getBrainData  a plot_brain3D )
    -- to by chtelo vyresit classem a privatni promennou fv
    -- getBrainData vraci plotInfo.fv, ktere se uklada do promenne brainsurface
    
    - pridane zpravy do assert
    - spousti se cele, bez pouziti bunek;
    -- COLIN27 BRAIN SLICES se docasne preskakuje
    -- return za 3D BRAIN MODEL program ukoncuje
    -- to se musi vyresit zvlastnima funkcema v class
    
    - osobni nastaveni Kamil - cele by to chtelo prevest do funkce, aby tam nebyly tyhle osobni nastaveni
    -- outputDir_p73, colorScale

--- main_brainPlot.m
+++ main_brainPlot.m
@@ -19,57 +19,68 @@ dir_toolbox = what('SEEG-visualization');     % folder 'visualization_SEEG' MUST
 if isempty(dir_toolbox) 
     error('Path to toolbox required. Add it to pathdef.m !'); 
 end
-addpath(genpath(dir_toolbox.path));
-dir_curr = pwd;
-cd(dir_toolbox.path);
-
+%addpath(genpath(dir_toolbox.path)); %kamil - path to be saved manually
+%dir_curr = pwd;
+%cd(dir_toolbox.path);
+outputDir_p73 ='d:\eeg\motol\pacienti\p073 Pech VT6\Aedist\figures\'; 
 %% ######################## USER INTERFACE ##################################
 % --- interface structure 'plotInfo'
 plotInfo = struct(...           % user interface structure: holds most (but not all!) of the user settings
-    'outputDir',[dir_toolbox.path filesep 'figures'], ...   % output directory, where the plots are saved
+    'outputDir',outputDir_p73, ...   % output directory, where the plots are saved
     'figureNamePrefix', 'myTest_', ...                  % figure name prefix. Program adds automatic suffix to each figure.
-    'figurePosition', [1 -479 2880 1472], ...           % position of a figure on screen. For whole screen, evoke a figure, maximize it and type: get(gcf, 'Position')
+    'figurePosition', [1281 -89 1920 964], ...           % position of a figure on screen. For whole screen, evoke a figure, maximize it and type: get(gcf, 'Position')
     'printResolution', 0, ...                           % choices: 0 (= screen resolution) or 600 (= dpi). Resolution of the figures.                          
-    'colorScale', [], ...                               % for example, [-10 10]. If empty, programs adjusts colorscale to 5 & 95 percentile of the data.
+    'colorScale', [0 0.7], ...                               % for example, [-10 10]. If empty, programs adjusts colorscale to 5 & 95 percentile of the data.
     'colorMap', jet(128), ...                           % colormap for channel values
     'MRI_fileDir', dir_toolbox.path, ...                % full path to brain MRI NIFTI (.nii) file. Must be normalized to MNI space!
-    'size_interpolate', 1.0, ...                        % in [mm], voxel size to which the brain is interpolated
+    'size_interpolate', 1.0, ...                        % in [mm], voxel size to which the brain is interpolated; 1.0 means no interpolation
     'size_coloredCube', 3.0, ...                        % in [mm], "voxel" size of the colored channel values
-    'slicePlanes', {{'axial','sagittal','coronal'}},... % planes to plot, options: 'axial','sagittal','coronal'
-    'model_views', [0 0; 0 89.999; 90 0], ...           % camera view angles at which the snapshot of the 3D model are taken. For some odd reason (camera light?), view(0,90) makes grey background...
-    'doAnimation_gif', true ...                         % GIF animation of 3D brain model (takes longer time)
+    ... %'slicePlanes', {{'axial','sagittal','coronal'}},... % planes to plot, options: 'axial','sagittal','coronal'
+    'slicePlanes', {{'sagittal','axial','coronal'}},... % planes to plot, options: 'axial','sagittal','coronal'
+    ... %'model_views', [ 0 89.999; 90 0; 0 0], ...           % camera view angles at which the snapshot of the 3D model are taken. For some odd reason (camera light?), view(0,90) makes grey background...
+    ... %'model_views', [90 0], ...           % camera view angles at which the snapshot of the 3D model are taken. For some odd reason (camera light?), view(0,90) makes grey background...
+    'doAnimation_gif', false, ...                         % GIF animation of 3D brain model (takes longer time)
+    'savefig',false,...                 %if save FIG figures 
+    'savepng',true, ...               %if sace png figures
+    'circle_size',56    ....   %size of the circles in 3D plot - 28  
 );
 
 % --- load channels MNI coors (variable 'data_channels' in 'channelsInfo.mat')
-load('channelsInfo.mat', 'data_channels');
-assert(exist('data_channels','var') == 1);
-plotInfo.chnls = data_channels;             % !!! set here your MNI coordinates as a structure array with the same fields as in this example!
+%load('channelsInfo.mat', 'data_channels');
+assert(exist('mni_channels','var') == 1,'MNI coordinates var mni_channels is missing');
+plotInfo.chnls = mni_channels;             % !!! set here your MNI coordinates as a structure array with the same fields as in this example!
 
 % --- channels values to plot, format = [channels x time]
-vals = randn(size(data_channels,2),2);      % !!! set here your channel valus, format = [channels x time], in this example: 2 time points of channel values
+%vals_channels = randn(size(data_channels,2),2);   % !!! set here your channel valus, format = [channels x time], in this example: 2 time points of channel values
+%vals_channels = ones(size(data_channels,2),1);    % vsechny elektrody stejnou barvou, jeden casovy okamzik
+assert(exist('vals_channels','var')==1,'channel values var vals_channels is missing');
 
 % --- channels color scale
 if isempty(plotInfo.colorScale)
-    clims = [prctile(vals(:),5), prctile(vals(:),95)]; clims = max(abs(clims)); 
+    clims = [prctile(vals_channels(:),5), prctile(vals_channels(:),95)]; clims = max(abs(clims)); 
     plotInfo.chnl_clims = [-clims, clims];
 else
     plotInfo.chnl_clims = plotInfo.colorScale;
 end
 
-
+if exist('brainsurface','var') && ~isempty(brainsurface)
+    plotInfo.fv = brainsurface;  %already computed isosurface - kamil
+end 
 
 %% ################## (A) COLIN27 BRAIN SLICES ##########################
 % colin27-specific: get brain template for slices
-plotInfo.plottingStyle = 'slices';
-plotInfo.MRI_file = [plotInfo.MRI_fileDir filesep 'wT1_subject.nii'];       % subject specific brain normalized to MNI space
-plotInfo.brain = getBrainData(plotInfo);
+if(false)
+    plotInfo.plottingStyle = 'slices';
+    plotInfo.MRI_file = [plotInfo.MRI_fileDir filesep 'wT1_subject.nii'];       % subject specific brain normalized to MNI space
+    plotInfo.brain = getBrainData(plotInfo);
 
-% colin27-specific: plot time-series of brain slices (1 figure / time point)
-plotInfo.outDir = [plotInfo.outputDir filesep 'slices_normalizedBrain'];
-for t = 1:size(vals,2)          % go thru all time points
-    plotInfo.figName = [plotInfo.figureNamePrefix 'colin_time' num2str(t)];
-    plot_brainSlices(vals(:,t), plotInfo);
-end    
+    % colin27-specific: plot time-series of brain slices (1 figure / time point)
+    plotInfo.outDir = [plotInfo.outputDir filesep 'slices_normalizedBrain'];
+    for t = 1:size(vals_channels,2)          % go through all time points
+        plotInfo.figName = [plotInfo.figureNamePrefix 'colin_time' num2str(t)];
+        plot_brainSlices(vals_channels(:,t), plotInfo);
+    end    
+end
 
 
 
@@ -79,16 +90,18 @@ end
 plotInfo.plottingStyle = '3D_model';
 plotInfo.size_interpolate = 1;          % consider larger voxels for faster & easier manipulation
 plotInfo.MRI_file = [plotInfo.MRI_fileDir filesep  'wc1T1_colin27.nii'];       % gray matter (segmented from colin27 by SPM12)
-plotInfo.brain = getBrainData(plotInfo);
+[plotInfo.brain plotInfo.fv] = getBrainData(plotInfo); %loads 3D brain MRI or CT = brain
+brainsurface = plotInfo.fv; %save for later use
 
 % colin27-specific: plot brain: 3D model 
 plotInfo.outDir = [plotInfo.outputDir filesep '3D_model'];
-for t = 1:size(vals,2)          % go thru all time points
+for t = 1:size(vals_channels,2)          % go thru all time points
+    disp(['Time point ' num2str(t)]);
     plotInfo.figName = [plotInfo.figureNamePrefix 'colin_time' num2str(t)];
-    plot_brain3D(vals(:,t), plotInfo);
+    plot_brain3D(vals_channels(:,t), plotInfo);
 end
 
-
+return; %dal uz nechci pokracovat
 
 %% ################## (C) SUBJECT-SPECIFIC SLICES ##########################
 % subject-specific: get brain info for slices
@@ -98,9 +111,9 @@ plotInfo.brain = getBrainData(plotInfo);
 
 % subject-specific: plot time-series of brain slices (1 figure / time point)
 plotInfo.outDir = [plotInfo.outputDir filesep 'slices_subjectBrain'];
-for t = 1:size(vals,2)          % go thru all time points
+for t = 1:size(vals_channels,2)          % go thru all time points
     plotInfo.figName = [plotInfo.figureNamePrefix 'subject_time' num2str(t)];
-    plot_brainSlices(vals(:,t), plotInfo);
+    plot_brainSlices(vals_channels(:,t), plotInfo);
 end    
 
 
